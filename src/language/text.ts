95c48c1ea24907b04b9b0cfbc80a30b68a22732b
frodehansen2 <frode.hansen@bekk.no>
2026-03-12 08:14:49 +0100

    Tekstendring -> attendance allowance (#31)

--- src/language/text.ts
+++ src/language/text.ts
@@ -2,11 +2,11 @@ export const text = {
     overskrift: {
         nb: 'Dine pleiepenger',
         nn: 'Dine pleiepenger',
-        en: 'Your nursing allowance',
+        en: 'Your attendance allowance',
     },
     underoverskrift: {
         nb: 'Oversikt over saken din',
         nn: 'Oversikt over saka di',
-        en: 'Navigate to your nursing allowance',
+        en: 'Navigate to your attendance allowance',
     },
 };

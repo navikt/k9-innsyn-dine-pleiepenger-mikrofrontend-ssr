682ddb3d63f4ee9ebcabc2f05db387cc6809f0b2
frodehansen2 <frode.hansen@bekk.no>
2026-06-09 10:35:29 +0200

    Upd

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,7 +1,6 @@
 import { getAnalyticsInstance } from '@navikt/nav-dekoratoren-moduler';
 
 export const logCardClick = async (url: string) => {
-    debugger;
     const logger = getAnalyticsInstance('dekoratoren');
     logger('navigere', { lenketekst: 'Dine pleiepenger', destinasjon: url });
 };

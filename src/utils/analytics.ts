62cfc7fd88c27681989787a68487010aea17ed4d
frodehansen2 <frode.hansen@bekk.no>
2026-06-09 10:55:12 +0200

    Pakkeoppdateringer (#90)

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,10 +1,7 @@
-import type { AnalyticsEvent } from '@navikt/nav-dekoratoren-moduler';
 import { getAnalyticsInstance } from '@navikt/nav-dekoratoren-moduler';
 
-type NavigereAnalyticsEvent = AnalyticsEvent<'navigere', { lenketekst: string; destinasjon: string }>;
-
-const analyticsLogger = getAnalyticsInstance<NavigereAnalyticsEvent>('k9-innsyn-dine-pleiepenger-mikrofrontend-ssr');
+const logger = getAnalyticsInstance('dekoratoren');
 
 export const logCardClick = async (url: string) => {
-    await analyticsLogger('navigere', { lenketekst: 'Dine pleiepenger', destinasjon: url });
+    logger('navigere', { lenketekst: 'Dine pleiepenger', destinasjon: url });
 };
